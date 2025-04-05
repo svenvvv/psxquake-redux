ec4c65694e62b1ef65e1bd32d0188916d82924cb
svenvvv <6577436+svenvvv@users.noreply.github.com>
2025-04-05 19:43:08 +0300

    Replace in-tree stdlib reimplementations with stdlib calls

--- include/common.h
+++ include/common.h
@@ -118,20 +118,20 @@ float MSG_ReadAngle(void);
 
 //============================================================================
 
-void Q_memset(void *dest, int fill, int count);
-void Q_memcpy(void *dest, void *src, int count);
-int Q_memcmp(void *m1, void *m2, int count);
-void Q_strcpy(char *dest, char *src);
-void Q_strncpy(char *dest, char *src, int count);
-int Q_strlen(char *str);
-char *Q_strrchr(char *s, char c);
-void Q_strcat(char *dest, char *src);
-int Q_strcmp(char *s1, char *s2);
-int Q_strncmp(char *s1, char *s2, int count);
-int Q_strcasecmp(char *s1, char *s2);
-int Q_strncasecmp(char *s1, char *s2, int n);
-int Q_atoi(char *str);
-float Q_atof(char *str);
+#define Q_memset memset
+#define Q_memcpy memcpy
+#define Q_memcmp memcmp
+#define Q_strcpy strcpy
+#define Q_strncpy strncpy
+#define Q_strlen strlen
+#define Q_strrchr strrchr
+#define Q_strcat strcat
+#define Q_strcmp strcmp
+#define Q_strncmp strncmp
+#define Q_strcasecmp strcasecmp
+#define Q_strncasecmp strncasecmp
+#define Q_atoi atoi
+#define Q_atof atof
 
 //============================================================================
 
