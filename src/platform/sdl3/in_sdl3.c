6a5223ccbf89de668c2c14b24f0a5334f9010716
svenvvv <6577436+svenvvv@users.noreply.github.com>
2025-04-05 21:04:07 +0300

    Add basic SDL3 keyboard input

--- src/platform/sdl3/in_sdl3.c
+++ src/platform/sdl3/in_sdl3.c
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "quakedef.h"
 
+#include <SDL3/SDL.h>
+
 void IN_Init(void)
 {
 }
@@ -31,6 +33,39 @@ void IN_Shutdown(void)
 
 void IN_Commands(void)
 {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type != SDL_EVENT_KEY_UP && event.type != SDL_EVENT_KEY_DOWN) {
+            continue;
+        }
+
+        SDL_Keycode sdl_key = event.key.key;
+        bool pressed = event.key.down;
+
+        Key_Event(K_SHIFT, event.key.mod & SDL_KMOD_SHIFT);
+
+        if (sdl_key == SDLK_ESCAPE) {
+            Key_Event(K_ESCAPE, pressed);
+        } else if (sdl_key == SDLK_TAB) {
+            Key_Event(K_TAB, pressed);
+        } else if (sdl_key == SDLK_RETURN) {
+            Key_Event(K_ENTER, pressed);
+        } else if (sdl_key == SDLK_SPACE) {
+            Key_Event(K_SPACE, pressed);
+        } else if (sdl_key == SDLK_BACKSPACE) {
+            Key_Event(K_BACKSPACE, pressed);
+        } else if (sdl_key == SDLK_UP) {
+            Key_Event(K_UPARROW, pressed);
+        } else if (sdl_key == SDLK_DOWN) {
+            Key_Event(K_DOWNARROW, pressed);
+        } else if (sdl_key == SDLK_LEFT) {
+            Key_Event(K_LEFTARROW, pressed);
+        } else if (sdl_key == SDLK_RIGHT) {
+            Key_Event(K_RIGHTARROW, pressed);
+        } else if (SDLK_0 <= sdl_key && sdl_key <= SDLK_Z) {
+            Key_Event('0' + event.key.key - SDLK_0, pressed);
+        }
+    }
 }
 
 void IN_Move(usercmd_t *cmd)
